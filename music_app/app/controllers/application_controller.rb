daaf204f1b9e597db202bc6aa950bbcc483f2f60
JhonJSC182 <jhonjsc182@hotmail.com>
2024-02-20 14:54:18 -0800

    finish CRRLLL

--- music_app/app/controllers/application_controller.rb
+++ music_app/app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+
+    def current_user
+        token = session[:session_token]
+        @current_user ||= User.find_by(session_token: token)
+    end
+
+    def require_logged_in
+        redirect_to new_session_url unless logged_in?
+    end
+
+    def require_logged_out
+        redirect_to new_user_url if logged_in?
+    end
+
+    def log_in(user)
+        session_token = user.reset_session_token
+        session[:session_token] = session_token
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def log_out
+        current_user.reset_session_token
+        session[:session_token] = nil
+        current_user = nil
+    end
 end
--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -1,5 +1,25 @@
 class SessionsController < ApplicationController
     def new
-
+        @user = User.new
+        render :new
     end
+
+    def create
+        email = params[:user][:email]
+        password = params[:user][:password]
+        @user = user.find_by_credentials(email, password)
+        if @user
+            log_in(@user)
+            redirect_to user_url(@user)
+        else
+            redirect_to new_user_url
+            flash[:error] = ["try again :p"]
+        end
+    end
+
+    def destroy
+        log_out
+        redirect_to new_session_url
+    end
+    
 end
